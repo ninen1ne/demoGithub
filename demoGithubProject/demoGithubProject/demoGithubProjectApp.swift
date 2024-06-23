23e5180e0a192843a92412a17c1d2d30abcd629f
Norawee Laohateerapong <StarlinzSama@gmail.com>
2024-06-23 10:04:15 +0700

    first commit

---
+++ demoGithubProject/demoGithubProject/demoGithubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoGithubProjectApp.swift
+//  demoGithubProject
+//
+//  Created by Norawee on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct demoGithubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
